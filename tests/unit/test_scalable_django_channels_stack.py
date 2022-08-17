87ee0a9660cb525cac46133fbf8d141876b7d86c
marianobrc <marianomartinez84@gmail.com>
2022-08-17 18:08:52 -0300

    Add CDK stacks for deployment

--- tests/unit/test_scalable_django_channels_stack.py
+++ tests/unit/test_scalable_django_channels_stack.py
@@ -1,10 +1,10 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from scalable_django_channels.scalable_django_channels_stack import ScalableDjangoChannelsStack
+from scalable_django_channels.my_django_chat_app_stack import ScalableDjangoChannelsStack
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in scalable_django_channels/scalable_django_channels_stack.py
+# resource in scalable_django_channels/my_django_chat_app_stack.py
 def test_sqs_queue_created():
     app = core.App()
     stack = ScalableDjangoChannelsStack(app, "scalable-django-channels")
