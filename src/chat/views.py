873f5f067d6eceb58f109e1505dab6ab96f67132
marianobrc <marianomartinez84@gmail.com>
2022-08-18 13:19:54 -0300

    Secure websockets with TLS in staging or production

--- src/chat/views.py
+++ src/chat/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 
 
@@ -6,6 +7,8 @@ def index(request):
 
 
 def room(request, room_name):
+    environment = os.environ.get("DJANGO_SETTINGS_MODULE", "config.settings.local")
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'ws_protocol': 'ws://' if environment == 'config.settings.local' else 'wss://'
     })
